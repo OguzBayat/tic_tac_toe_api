fd19c9ca4c469257c2df5af10c7e370f9138d383
OguzBayat <oguzbayat77@gmail.com>
2022-12-04 14:06:29 +0300

    Create player model

---
+++ app/models/player.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Player < ApplicationRecord
+  enum sign: { X: 0, O: 1 }.freeze
+
+  def player_who_has_x_sign?
+    sign == 'X'
+  end
+
+  def player_who_has_o_sign?
+    sign == 'O'
+  end
+end
