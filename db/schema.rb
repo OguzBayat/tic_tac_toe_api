6df61934e3fd45a65b0335160cea82d0471bf45a
Oğuz Bayat <oguzbayat77@gmail.com>
2022-12-04 16:11:23 +0300

    Use postgres instead sqlite (#7)

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_04_114141) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer "winner_id"
     t.integer "move_count", default: 0
