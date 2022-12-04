ab2a647831d0b762bc36a7d4830ab95bde431999
Oğuz Bayat <oguzbayat77@gmail.com>
2022-12-04 17:02:14 +0300

    Create winning_numbers model for game-winning pattern (#8)
    * Create winning_numbers model for game-winning pattern

---
+++ app/controllers/winning_numbers_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class WinningNumbersController < ApplicationController
+  before_action :set_winning_number, only: %i[show update destroy]
+
+  # GET /winning_numbers
+  def index
+    @winning_numbers = WinningNumber.all
+
+    render json: @winning_numbers
+  end
+
+  # GET /winning_numbers/1
+  def show
+    render json: @winning_number
+  end
+
+  # POST /winning_numbers
+  def create
+    @winning_number = WinningNumber.new(winning_number_params)
+
+    if @winning_number.save
+      render json: @winning_number, status: :created, location: @winning_number
+    else
+      render json: @winning_number.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /winning_numbers/1
+  def update
+    if @winning_number.update(winning_number_params)
+      render json: @winning_number
+    else
+      render json: @winning_number.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /winning_numbers/1
+  def destroy
+    @winning_number.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_winning_number
+    @winning_number = WinningNumber.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def winning_number_params
+    params.require(:winning_number).permit(:numbers)
+  end
+end
