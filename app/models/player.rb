ce4a6774ef64d0eca09b7bd7c03e90b5acba1e95
OguzBayat <oguzbayat77@gmail.com>
2022-12-04 14:11:44 +0300

    add validation to player model fields

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Player < ApplicationRecord
+  validates :name, presence: true
+  validates :sign, presence: true
   enum sign: { X: 0, O: 1 }.freeze
 
   def player_who_has_x_sign?
