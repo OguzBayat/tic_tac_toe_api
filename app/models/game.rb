5a8ab8eb565f91dcd4791d438abe4900e698b624
OguzBayat <oguzbayat77@gmail.com>
2023-01-02 01:57:39 +0300

    Add after commit and set winner logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,11 +6,45 @@ class Game < ApplicationRecord
   has_many :plays
 
   after_commit :create_plays, on: :create
+  after_commit :update_plays, on: :update
 
   def create_plays
     player1 = Player.where(sign: 'X').first
     player2 = Player.where(sign: 'O').first
     Play.create!(game: self, player: player1)
     Play.create!(game: self, player: player2)
+  end
+
+  def update_plays
+    player1_play = plays.where(player_id: 1)
+    player2_play = plays.where(player_id: 2)
+
+    player1_play.update(moves: player1_moves)
+    player2_play.update(moves: player2_moves)
+    set_winner
+  end
+
+  private
+
+  def set_winner
+    if find_winner(plays.first) == 1
+      update_columns(winner_id: 1)
+    elsif find_winner(plays.last) == 2
+      update_columns(winner_id: 2)
+    end
+  end
+
+  def find_winner(play)
+    moves = play.moves.freeze
+    return if moves.count < 3
+
+    moves_arr = play.moves.combination(3).to_a
+    winning_numbers = WinningNumber.all.pluck(:numbers)
+
+    winning_numbers.each do |wn|
+      moves_arr.each do |ma|
+        return play.player_id if wn.difference(ma).blank?
+      end
+    end
   end
 end
